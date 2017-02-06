9a79ef0f4f1793f7cf202724b5ebf4bb20828f34
mschimmer <markus.schimmer@gmail.com>
2017-02-06 21:53:47 +0100

    Added Java Faker Support

--- src/main/java/mschimmer/dev/Sandbox.java
+++ src/main/java/mschimmer/dev/Sandbox.java
@@ -1,5 +1,7 @@
 package mschimmer.dev;
 
+import com.github.javafaker.Faker;
+
 /**
  * Created by markus on 05.02.17.
  */
@@ -8,6 +10,12 @@ public class Sandbox {
     public static void main(String[] args){
 
         System.out.println("Hello World");
+
+        Faker faker = new Faker();
+
+        String name = faker.name().fullName();
+
+        System.out.println(name);
 
     }
 }
